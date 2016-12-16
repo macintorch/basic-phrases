eb26aa2272c19dce046a3bb4a59f9ae82d079e7f
Ainor Syahrizal <macintorch@gmail.com>
2016-12-17 03:24:01 +0800

    test button tapped"

--- app/src/main/java/ainor/com/my/basicphrases/MainActivity.java
+++ app/src/main/java/ainor/com/my/basicphrases/MainActivity.java
@@ -2,6 +2,8 @@ package ainor.com.my.basicphrases;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonTapped (View view) {
+
+        int id = view.getId();
+        String ourId = "";
+        ourId = view.getResources().getResourceEntryName(id);
+
+        Log.i("Button Tapped", ourId);
     }
 }
